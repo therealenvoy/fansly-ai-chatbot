c79a819d8fe0a9f4979d08644c3cd5daa8af24d1
therealenvoy1 <theonlyenvoy1@pm.me>
2026-07-28 10:48:03 +0700

    Protect provider request log privacy

--- src/main.py
+++ src/main.py
@@ -67,6 +67,8 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
 )
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logging.getLogger("httpcore").setLevel(logging.WARNING)
 logger = logging.getLogger("fansly-bot")
 
 
